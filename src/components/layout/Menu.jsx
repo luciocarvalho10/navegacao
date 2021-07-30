6192b38d72bcd90fe296e9a9fb3cfc9bda16a452
Lucio <luciocarvalho10@hotmail.com.br>
2021-07-30 16:49:53 -0400

    Aplicando estilos

--- src/components/layout/Menu.jsx
+++ src/components/layout/Menu.jsx
@@ -3,7 +3,12 @@ import React from 'react'
 
 const Menu = props => (
   <aside className="Menu">
-    Componente Menu
+    <nav>
+      <ul>
+        <li><a href="/">Início</a></li>
+        <li><a href="/">Sobre</a></li>
+      </ul>
+    </nav>
   </aside>
 )
 
